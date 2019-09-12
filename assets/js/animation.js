673cc8e4b8ad8fd463ea8ce94fe0a16840afe9e4
Johan Åkerman <44551216+johan-akerman@users.noreply.github.com>
2019-09-12 21:54:29 +0200

    Add files via upload

---
+++ assets/js/animation.js
@@ -0,0 +1,243 @@
+        $(document).ready(function ()
+        {
+
+
+            // hero
+            $('#hero-title').css('opacity', 0);
+
+            $('#hero-title').waypoint(function ()
+            {
+                $('#hero-title').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+
+            // hero
+            $('#hero-image').css('opacity', 0);
+
+            $('#hero-image').waypoint(function ()
+            {
+                $('#hero-image').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+
+            // hero
+            $('#hero-text').css('opacity', 0);
+
+            $('#hero-text').waypoint(function ()
+            {
+                $('#hero-text').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+
+
+            // hero
+            $('.animated-btn').css('opacity', 0);
+
+            $('.animated-btn').waypoint(function ()
+            {
+                $('.animated-btn').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+            // hero
+            $('#what-we-do-btn').css('opacity', 0);
+
+            $('#what-we-do-btn').waypoint(function ()
+            {
+                $('#what-we-do-btn').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+            // hero
+            $('#what-we-do-content-title').css('opacity', 0);
+
+            $('#what-we-do-content-title').waypoint(function ()
+            {
+                $('#what-we-do-content-title').addClass('fadeInUp');
+            },
+            {
+                offset: '80%'
+            });
+
+            $('#what-we-do-1').css('opacity', 0);
+
+            $('#what-we-do-1').waypoint(function ()
+            {
+                $('#what-we-do-1').addClass('fadeInUp');
+            },
+            {
+                offset: '90%'
+            });
+
+            $('#what-we-do-2').css('opacity', 0);
+
+            $('#what-we-do-2').waypoint(function ()
+            {
+                $('#what-we-do-2').addClass('fadeInUp');
+            },
+            {
+                offset: '90%'
+            });
+
+            $('#what-we-do-3').css('opacity', 0);
+
+            $('#what-we-do-3').waypoint(function ()
+            {
+                $('#what-we-do-3').addClass('fadeInUp');
+            },
+            {
+                offset: '90%'
+            });
+
+            $('#what-we-do-4').css('opacity', 0);
+
+            $('#what-we-do-4').waypoint(function ()
+            {
+                $('#what-we-do-4').addClass('fadeInUp');
+            },
+            {
+                offset: '90%'
+            });
+
+            $('.portfolio-title').css('opacity', 0);
+
+            $('.portfolio-title').waypoint(function ()
+            {
+                $('.portfolio-title').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+            $('.portfolio-text').css('opacity', 0);
+
+            $('.portfolio-text').waypoint(function ()
+            {
+                $('.portfolio-text').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+            $('.previous-work-1').css('opacity', 0);
+
+            $('.previous-work-1').waypoint(function ()
+            {
+                $('.previous-work-1').addClass('fadeInUp');
+            },
+            {
+                offset: '120%'
+            });
+
+            $('.previous-work-2').css('opacity', 0);
+
+            $('.previous-work-2').waypoint(function ()
+            {
+                $('.previous-work-2').addClass('fadeInUp');
+            },
+            {
+                offset: '120%'
+            });
+
+            $('.previous-work-3').css('opacity', 0);
+
+            $('.previous-work-3').waypoint(function ()
+            {
+                $('.previous-work-3').addClass('fadeInUp');
+            },
+            {
+                offset: '120%'
+            });
+
+            $('.previous-work-4').css('opacity', 0);
+
+            $('.previous-work-4').waypoint(function ()
+            {
+                $('.previous-work-4').addClass('fadeInUp');
+            },
+            {
+                offset: '140%'
+            });
+
+
+
+
+            // hero
+            $('#contact-title').css('opacity', 0);
+
+            $('#contact-title').waypoint(function ()
+            {
+                $('#contact-title').addClass('fadeInUp');
+            },
+            {
+                offset: '90%'
+            });
+
+
+
+
+
+            // hero
+            $('#contact-text').css('opacity', 0);
+
+            $('#contact-text').waypoint(function ()
+            {
+                $('#contact-text').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+            // hero
+            $('#contact-info-upper').css('opacity', 0);
+
+            $('#contact-info-upper').waypoint(function ()
+            {
+                $('#contact-info-upper').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+
+            // hero
+            $('#contact-info-down').css('opacity', 0);
+
+            $('#contact-info-down').waypoint(function ()
+            {
+                $('#contact-info-down').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+
+            // hero
+            $('.about-us-text').css('opacity', 0);
+
+            $('.about-us-text').waypoint(function ()
+            {
+                $('.about-us-text').addClass('fadeInUp');
+            },
+            {
+                offset: '100%'
+            });
+
+
+
+
+
+        });
